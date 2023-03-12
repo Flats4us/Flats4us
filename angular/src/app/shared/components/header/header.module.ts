82977bbea6a2769a974b602fa23bf2f6d4ce4cbb
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-03-12 14:55:36 +0100

    header-navbar

--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
