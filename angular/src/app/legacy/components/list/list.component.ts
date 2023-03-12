28e27a71525f67fcd772ba55e60326ab21c0619e
mattonaly <mattioskar.mat@gmail.com>
2023-03-12 13:20:31 +0100

    Pipeline fix

--- angular/src/app/legacy/components/list/list.component.ts
+++ angular/src/app/legacy/components/list/list.component.ts
@@ -2,43 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { TenantsService } from 'src/app/services/tenants.service';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit {
+	tenants: any;
+	currentTenant: any;
 
-  tenants: any;
-  currentTenant: any;
+	constructor(private tenantsService: TenantsService) {}
 
-  constructor(private tenantsService: TenantsService) {  }
+	ngOnInit(): void {
+		this.getAllTenants();
+	}
 
-  ngOnInit(): void {
-    this.getAllTenants();
-  }
+	getAllTenants(): void {
+		this.tenantsService.list().subscribe(
+			(tenants: any) => {
+				this.tenants = tenants;
+			},
+			(error: any) => {
+				console.log(error);
+			}
+		);
+	}
 
-  getAllTenants(): void {
-    this.tenantsService.list()
-      .subscribe(
-        (tenants: any) => {
-          this.tenants = tenants;
-        },
-        (error: any) => {
-          console.log(error);
-        }
-      );
-  }
-
-  deleteTenant(id: number) {
-    this.tenantsService.delete(id)
-      .subscribe(
-        response => {
-          this.getAllTenants();
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
-
+	deleteTenant(id: number) {
+		this.tenantsService.delete(id).subscribe(
+			(response) => {
+				this.getAllTenants();
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}
 }
