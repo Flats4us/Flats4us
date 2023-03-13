e18624fb2132f5d34a8eb36480e0bd71c404f821
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-03-13 20:44:04 +0100

    navbar

--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -5,7 +5,6 @@ import { HeaderComponent } from './header.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
-
 @NgModule({
 	declarations: [HeaderComponent],
 	imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
