2dea659a9a7a63f0a28e00fbc2eb31a225a73fb8
Jakub Gizewski <s22523@pjwstk.edu.pl>
2023-03-14 23:24:10 +0100

    wip

---
+++ Flats4us/Entities/Property.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Flats4us.Entities
+{
+    [Table("Property")]
+    public class Property
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+
+        public int Surface { get; set; }
+
+        public int MaxInhabitants { get; set; }
+
+
+    }
+}
