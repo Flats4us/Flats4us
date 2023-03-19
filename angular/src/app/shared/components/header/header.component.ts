cdf9eff96c60559b2c7ca25ee8fe755155bcbe78
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-03-19 21:00:36 +0100

    navbar(BM)

--- angular/src/app/shared/components/header/header.component.ts
+++ angular/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+	trigger,
+	state,
+	style,
+	transition,
+	animate,
+} from '@angular/animations';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
+	animations: [
+		trigger('sidenav', [
+			state(
+				'open',
+				style({
+					transform: 'translateX(0)',
+				})
+			),
+			state(
+				'closed',
+				style({
+					transform: 'translateX(-100%)',
+				})
+			),
+			transition('open => closed', [animate('0.2s')]),
+			transition('closed => open', [animate('0.2s')]),
+		]),
+	],
 })
 export class HeaderComponent {
+	showMenu = false;
 	isUserLoggedIn = true;
+
+	toggleMenu() {
+		this.showMenu = !this.showMenu;
+	}
 }
--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -5,9 +5,21 @@ import { HeaderComponent } from './header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+	imports: [
+		CommonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		MatListModule,
+	],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
