b910df0bdbe61b9067f2df442293387155b0a0bd
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-03-19 22:07:38 +0100

    Update header.component.ts

--- angular/src/app/shared/components/header/header.component.ts
+++ angular/src/app/shared/components/header/header.component.ts
@@ -10,25 +10,8 @@ import {
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss'],
-	animations: [
-		trigger('sidenav', [
-			state(
-				'open',
-				style({
-					transform: 'translateX(0)',
-				})
-			),
-			state(
-				'closed',
-				style({
-					transform: 'translateX(-100%)',
-				})
-			),
-			transition('open => closed', [animate('0.2s')]),
-			transition('closed => open', [animate('0.2s')]),
-		]),
-	],
+	styleUrls: ['./header.component.scss']
+
 })
 export class HeaderComponent {
 	showMenu = false;
