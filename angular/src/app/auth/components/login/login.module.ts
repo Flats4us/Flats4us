1ddc93361003d74fc43aacfae68152b52a858b3f
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-03-19 20:22:11 +0100

    Login component added

--- angular/src/app/auth/components/login/login.module.ts
+++ angular/src/app/auth/components/login/login.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
-	declarations: [LoginComponent],
-	imports: [CommonModule],
+  declarations: [LoginComponent],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule],
 })
 export class LoginModule {}
