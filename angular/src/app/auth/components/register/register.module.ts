447f3870176afca84edb131e5e34e1dc460e1916
Jakub Giżewski <76437687+giziuuu@users.noreply.github.com>
2023-03-20 18:12:02 +0100

    #13 Register page (#15)
    * Register page
    
    * Prettier
    
    * Prettier 2
    
    * fix: lazy loading auth module
    
    * Register page
    
    * Prettier
    
    * Prettier 2
    
    * Removed import
    
    * Removed import again
    
    * styling changed
    
    ---------
    
    Co-authored-by: giziuuu <kubagizewski@gmail.com>
    Co-authored-by: mattonaly <mattioskar.mat@gmail.com>

--- angular/src/app/auth/components/register/register.module.ts
+++ angular/src/app/auth/components/register/register.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
 	declarations: [RegisterComponent],
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		MatButtonModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+	],
 })
 export class RegisterModule {}
