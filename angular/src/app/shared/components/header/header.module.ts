14cfd945e49ef9a9cd9cf3b17940539ed3044c34
Matthew <68421340+mattonaly@users.noreply.github.com>
2023-03-20 18:10:35 +0100

    Navbar (#10)
    * header-navbar
    
    * Update header.component.html
    
    * nb
    
    * nb
    
    * navbar
    
    * navbar
    
    * navbar(BM)
    
    * navbar(BM)
    
    * Update header.component.ts
    
    * NAV
    
    ---------
    
    Co-authored-by: Smurf228 <109930207+Smurf228@users.noreply.github.com>

--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -2,10 +2,24 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		MatListModule,
+	],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
