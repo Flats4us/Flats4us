d8f65087625a968dffac91bd24ea5ccfa7b66302
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-03-25 21:53:00 +0100

    nb2.0

--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -8,26 +8,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
-import { MatMenuModule } from '@angular/material/menu';
+import {MatRadioModule} from "@angular/material/radio";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [
-		CommonModule,
-		MatToolbarModule,
-		MatIconModule,
-		MatButtonModule,
-		BrowserAnimationsModule,
-		MatSidenavModule,
-		MatListModule,
-		MatRadioModule,
-		MatSlideToggleModule,
-		FormsModule,
-		MatMenuModule,
-	],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+  ],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
