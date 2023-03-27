f1ef7f501df7c87789b7252d446c701350f2b619
giziuuu <kubagizewski@gmail.com>
2023-03-27 03:48:26 +0200

    wip

--- Flats4us/Entities/Moderator.cs
+++ Flats4us/Entities/Moderator.cs
@@ -1,6 +1,15 @@
 ﻿namespace Flats4us.Entities
 {
+    public enum Department
+    {
+        dept1,
+        dept2
+    }
+
     public class Moderator
     {
+        public DateTime HireDate { get; set; }
+
+        public Department Department { get; set; }
     }
 }
