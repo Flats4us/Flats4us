fc768ba1d02d449a9f1da2fe87217b150ff1f462
mattonaly <mattioskar.mat@gmail.com>
2023-04-01 19:41:27 +0200

    feat: authorization, update rules

--- angular/src/app/shared/services/auth.service.spec.ts
+++ angular/src/app/shared/services/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TenantsService } from './tenants.service';
+import { AuthService } from './auth.service';
 
-describe('TenantsService', () => {
-	let service: TenantsService;
+describe('AuthService', () => {
+	let service: AuthService;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({});
-		service = TestBed.inject(TenantsService);
+		service = TestBed.inject(AuthService);
 	});
 
 	it('should be created', () => {
