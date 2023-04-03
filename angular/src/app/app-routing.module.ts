6fb344d01eb5ad68df8520f01b5febacf8f394ee
Konrad Sochacki <s23801@pjwstk.edu.pl>
2023-04-03 19:52:33 +0200

    Pierwszy commit

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainSiteComponent } from './main-site/main-site/main-site.component';
 
 const routes: Routes = [
+	{ path: 'start', component: MainSiteComponent },
 	{
 		path: 'auth',
 		loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
