5a66015d15a8cba3cfa48728e59105da1f5aeb73
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-04-15 14:34:19 +0200

    #29 Zmiana hasła

--- angular/src/app/emailChange/emailChange.component.ts
+++ angular/src/app/emailChange/emailChange.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmailChangeComponent {
-  hide = true;
+	hide = true;
 }
