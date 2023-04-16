4381ef3338b3044ea9f0ad26e91559e16acf890b
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-04-16 12:39:20 +0200

    #29 Zmiana hasła

---
+++ angular/src/app/legacy/components/create/create.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+import { TenantsService } from '../../services/tenants.service';
+
+@Component({
+	selector: 'app-create',
+	templateUrl: './create.component.html',
+	styleUrls: ['./create.component.scss'],
+})
+export class CreateComponent {
+	tenant = {
+		name: '',
+		surname: '',
+		addressLine1: '',
+		addressLine2: '',
+		addressLine3: '',
+		email: '',
+		phoneNumber: '',
+	};
+	isTenantAdded = false;
+
+	constructor(private tenantsService: TenantsService) {}
+
+	addTenant(): void {
+		const data = {
+			name: this.tenant.name,
+			surname: this.tenant.surname,
+			addressLine1: this.tenant.addressLine1,
+			addressLine2: this.tenant.addressLine2,
+			addressLine3: this.tenant.addressLine3,
+			email: this.tenant.email,
+			phoneNumber: this.tenant.phoneNumber,
+		};
+
+		this.tenantsService.create(data).subscribe(
+			(response) => {
+				console.log(response);
+				this.isTenantAdded = true;
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}
+
+	newTenant(): void {
+		this.isTenantAdded = false;
+		this.tenant = {
+			name: '',
+			surname: '',
+			addressLine1: '',
+			addressLine2: '',
+			addressLine3: '',
+			email: '',
+			phoneNumber: '',
+		};
+	}
+}
---
+++ angular/src/app/legacy/components/list/list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TenantsService } from 'src/app/services/tenants.service';
+
+@Component({
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+	tenants: any;
+	currentTenant: any;
+
+	constructor(private tenantsService: TenantsService) {}
+
+	ngOnInit(): void {
+		this.getAllTenants();
+	}
+
+	getAllTenants(): void {
+		this.tenantsService.list().subscribe(
+			(tenants: any) => {
+				this.tenants = tenants;
+			},
+			(error: any) => {
+				console.log(error);
+			}
+		);
+	}
+
+	deleteTenant(id: number) {
+		this.tenantsService.delete(id).subscribe(
+			(response) => {
+				this.getAllTenants();
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}
+}
