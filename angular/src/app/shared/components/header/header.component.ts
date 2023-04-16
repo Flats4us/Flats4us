20610d1de6b184ceddca2280a4a3cf1800b35897
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-04-16 19:12:26 +0200

    navbr2

--- angular/src/app/shared/components/header/header.component.ts
+++ angular/src/app/shared/components/header/header.component.ts
@@ -8,5 +8,18 @@ import { Router } from '@angular/router';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-	}
+  isUserLoggedIn = true;
+  isUserLoggedInAsStudent = true;
+
+  constructor(private router: Router) {}
+
+  logIn() {
+    this.router.navigate(['auth/login']);
+  }
+  signIn() {
+    this.router.navigate(['auth/register']);
+  }
+  profile() {
+    this.router.navigate(['profile/profile'])
+  }
 }
