c074fcac32f0c637c3dac465706e2c474578c02a
Bartosz Tul <101190942+s22794@users.noreply.github.com>
2023-04-23 16:39:55 +0200

    Revert "#23 main page"

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainSiteComponent } from './main-site/main-site/main-site.component';
 
 const routes: Routes = [
-	{ path: 'start', component: MainSiteComponent },
 	{
 		path: 'auth',
 		loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
