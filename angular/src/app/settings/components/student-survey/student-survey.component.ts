44f7d3da3e85cea213594a342016c2a93865b82f
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-04-24 00:09:56 +0200

    #35 Ankieta studenta

--- angular/src/app/settings/components/student-survey/student-survey.component.ts
+++ angular/src/app/settings/components/student-survey/student-survey.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	OnInit,
+	Input,
+} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { IJsonFormData } from '../json-form/json-form.component';
 
 @Component({
 	selector: 'app-student-survey',
@@ -8,20 +15,35 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 	styleUrls: ['./student-survey.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StudentSurveyComponent {
+export class StudentSurveyComponent implements OnInit {
 	public studentSurveyForm: FormGroup;
 	public partyy = 1;
 	public tidy!: number;
 	public smoke!: number;
 	public social!: number;
 
-	constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+	public formData!: IJsonFormData;
+
+	constructor(
+		private fb: FormBuilder,
+		private snackBar: MatSnackBar,
+		private http: HttpClient
+	) {
 		this.studentSurveyForm = this.fb.group({
 			party: 0,
 		});
 		this.studentSurveyForm.controls['party'].valueChanges.subscribe((value) => {
 			this.partyy = value;
 		});
+	}
+
+	public ngOnInit(): void {
+		this.http
+			.get<IJsonFormData>('../../assets/survey.json')
+			.subscribe((formData: IJsonFormData) => {
+				console.log(formData);
+				this.formData = formData;
+			});
 	}
 
 	public onSubmit() {
