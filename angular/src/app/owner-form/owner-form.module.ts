38ca35f98f4e0d538e78130c5e02265d92da6063
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-04-23 20:18:39 +0200

    ownerForm#1

--- angular/src/app/owner-form/owner-form.module.ts
+++ angular/src/app/owner-form/owner-form.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from "@angular/material/card";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {OwnerFormComponent} from "./owner-form.component";
-import {FormsModule} from "@angular/forms";
-
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { OwnerFormComponent } from './owner-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [OwnerFormComponent],
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatRadioModule,
-    MatCheckboxModule,
-    FormsModule,
-  ],
-  exports: [OwnerFormComponent],
+	declarations: [OwnerFormComponent],
+	imports: [
+		CommonModule,
+		MatCardModule,
+		MatRadioModule,
+		MatCheckboxModule,
+		FormsModule,
+	],
+	exports: [OwnerFormComponent],
 })
-export class OwnerFormModule { }
+export class OwnerFormModule {}
