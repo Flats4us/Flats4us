49779424052bcc823d5f8ca43d49064c4ba97e9e
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-04-24 00:09:56 +0200

    #35 Ankieta studenta

--- angular/src/app/settings/settings-routing.module.ts
+++ angular/src/app/settings/settings-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { EmailChangeComponent } from './components/emailChange/emailChange.component';
-import { PasswordChangeComponent } from './components/passwordChange/passwordChange.component';
 import { StudentSurveyComponent } from './components/student-survey/student-survey.component';
 
 const routes: Routes = [
-	{
-		path: 'email-change',
-		component: EmailChangeComponent,
-	},
-	{
-		path: 'password-change',
-		component: PasswordChangeComponent,
-	},
 	{
 		path: 'student-survey',
 		component: StudentSurveyComponent,
