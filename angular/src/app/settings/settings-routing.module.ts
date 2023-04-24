4724a45dfde2efa40b299f6ee6e0a56ec2c51ff8
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2023-04-24 15:33:36 +0200

    * #28 Zmiana maila
    * #28 Zmiana maila
    
    * #28 Zmiana maila
    
    * #28 Zmiana maila
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * fix: no console
    
    * feat: authorization, update rules (#30)
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * Revert "#28 Zmiana maila"
    
    This reverts commit 84609a54198541591f56f1a7ac460f622c094b93.
    
    * Revert "#28 Zmiana maila"
    
    This reverts commit 5c219a01ac8b11702ae3f9af705613e5223a5f43.
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * #28 Zmiana maila
    
    * #28 Zmiana maila
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #29 Zmiana hasła & #28 Zmiana maila
    
    * #35 Ankieta studenta
    
    ---------
    
    Co-authored-by: mattonaly <68421340+mattonaly@users.noreply.github.com>

--- angular/src/app/settings/settings-routing.module.ts
+++ angular/src/app/settings/settings-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { StudentSurveyComponent } from './components/student-survey/student-survey.component';
+import { EmailChangeComponent } from './components/emailChange/emailChange.component';
+import { PasswordChangeComponent } from './components/passwordChange/passwordChange.component';
 
 const routes: Routes = [
+  {
+    path: 'student-survey',
+    component: StudentSurveyComponent,
+  },
 	{
-		path: 'student-survey',
-		component: StudentSurveyComponent,
+		path: 'email-change',
+		component: EmailChangeComponent,
+	},
+	{
+		path: 'password-change',
+		component: PasswordChangeComponent,
 	},
 ];
 
