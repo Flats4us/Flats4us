5a5df609079da9cc6c273adea5bdc44bf639a7d1
Konrad Sochacki <s23801@pjwstk.edu.pl>
2023-04-28 23:36:44 +0200

    PoprawionyRevert5

--- angular/src/app/main-site/main-site/main-site.component.ts
+++ angular/src/app/main-site/main-site/main-site.component.ts
@@ -5,16 +5,16 @@ import { Observable } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { IArea } from "../../shared/models/main-site.models";
-import { ICitiesGroup } from "../../shared/models/main-site.models";
-import { IDistance } from "../../shared/models/main-site.models";
-import { IFloor } from "../../shared/models/main-site.models";
-import { IPrice } from "../../shared/models/main-site.models";
-import { IProperty } from "../../shared/models/main-site.models";
-import { IRegion } from "../../shared/models/main-site.models";
-import { IRoom } from "../../shared/models/main-site.models";
-import { IYear } from "../../shared/models/main-site.models";
-import { RegionCity } from "../../shared/models/main-site.models";
+import { IArea } from '../../shared/models/main-site.models';
+import { ICitiesGroup } from '../../shared/models/main-site.models';
+import { IDistance } from '../../shared/models/main-site.models';
+import { IFloor } from '../../shared/models/main-site.models';
+import { IPrice } from '../../shared/models/main-site.models';
+import { IProperty } from '../../shared/models/main-site.models';
+import { IRegion } from '../../shared/models/main-site.models';
+import { IRoom } from '../../shared/models/main-site.models';
+import { IYear } from '../../shared/models/main-site.models';
+import { RegionCity } from '../../shared/models/main-site.models';
 
 export const filter = (opt: string[], value: string): string[] => {
 	const filterValue = value.toLowerCase();
@@ -28,18 +28,15 @@ export const filter = (opt: string[], value: string): string[] => {
 	styleUrls: ['./main-site.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class MainSiteComponent implements OnInit {
-
 	public showMoreFilters = false;
 	public numberOfRecords = 143084;
 	public selectedRegion = '';
 
 	public showFilters() {
-		if(this.showMoreFilters) {
+		if (this.showMoreFilters) {
 			this.showMoreFilters = false;
-		}
-		else {
+		} else {
 			this.showMoreFilters = true;
 		}
 	}
@@ -159,27 +156,30 @@ export class MainSiteComponent implements OnInit {
 	}
 
 	public ngOnInit() {
-		
 		this.citiesForm
-		.get('regionsGroup')
-		?.valueChanges.subscribe((selectedValue) =>
-		selectedValue === null ? (this.selectedRegion = '') : (this.selectedRegion = selectedValue));
+			.get('regionsGroup')
+			?.valueChanges.subscribe((selectedValue) =>
+				selectedValue === null
+					? (this.selectedRegion = '')
+					: (this.selectedRegion = selectedValue)
+			);
 
 		this.citiesGroupOptions = this.citiesForm
-		.get('citiesGroup')!
-		.valueChanges.pipe(
-			startWith(''),
-			map((value) => this.filterGroup(value || ''))
-		);
+			.get('citiesGroup')!
+			.valueChanges.pipe(
+				startWith(''),
+				map((value) => this.filterGroup(value || ''))
+			);
 	}
 
 	private filterGroup(value: string): ICitiesGroup[] {
 		return this.citiesGroups
-		.map((group) => ({
-			region: group.region,
-			cities: filter(group.cities, value)
-		})).filter(
-			(group) => group.cities.length > 0 && group.region === this.selectedRegion
+			.map((group) => ({
+				region: group.region,
+				cities: filter(group.cities, value),
+			}))
+			.filter(
+				(group) => group.cities.length > 0 && group.region === this.selectedRegion
 			);
 	}
 
@@ -203,7 +203,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 'świętokrzyskie' },
 		{ value: 'warmińsko-mazurskie' },
 		{ value: 'wielkopolskie' },
-		{ value: 'zachodniopomorskie' }
+		{ value: 'zachodniopomorskie' },
 	];
 
 	public areaFroms: IArea[] = [
@@ -213,7 +213,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 60, viewValue: '60 m²' },
 		{ value: 80, viewValue: '80 m²' },
 		{ value: 100, viewValue: '100 m²' },
-		{ value: 120, viewValue: '120 m²' }
+		{ value: 120, viewValue: '120 m²' },
 	];
 	public areaTos: IArea[] = [
 		{ value: 20, viewValue: '20 m²' },
@@ -222,7 +222,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 80, viewValue: '80 m²' },
 		{ value: 100, viewValue: '100 m²' },
 		{ value: 120, viewValue: '120 m²' },
-		{ value: 140, viewValue: '140 m²' }
+		{ value: 140, viewValue: '140 m²' },
 	];
 	public priceMaxs: IPrice[] = [
 		{ value: 1000, viewValue: '1000 zł' },
@@ -231,7 +231,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 4000, viewValue: '4000 zł' },
 		{ value: 5000, viewValue: '5000 zł' },
 		{ value: 6000, viewValue: '6000 zł' },
-		{ value: 7000, viewValue: '7000 zł' }
+		{ value: 7000, viewValue: '7000 zł' },
 	];
 	public numberOfRooms: IRoom[] = [
 		{ value: 1, viewValue: '1' },
@@ -240,7 +240,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 4, viewValue: '4' },
 		{ value: 5, viewValue: '5' },
 		{ value: 6, viewValue: '6' },
-		{ value: 7, viewValue: '7' }
+		{ value: 7, viewValue: '7' },
 	];
 	public distances: IDistance[] = [
 		{ value: 0, viewValue: '0 km' },
@@ -249,7 +249,7 @@ export class MainSiteComponent implements OnInit {
 		{ value: 15, viewValue: '15 km' },
 		{ value: 25, viewValue: '25 km' },
 		{ value: 50, viewValue: '50 km' },
-		{ value: 75, viewValue: '75 km' }
+		{ value: 75, viewValue: '75 km' },
 	];
 	public numberOfFloors: IFloor[] = [
 		{ value: 1, viewValue: '1' },
@@ -260,19 +260,17 @@ export class MainSiteComponent implements OnInit {
 		{ value: 10, viewValue: '10' },
 		{ value: 20, viewValue: '20' },
 		{ value: 50, viewValue: '40' },
-		{ value: 100, viewValue: '80' }
+		{ value: 100, viewValue: '80' },
 	];
 	public yearOfBuilds: IYear[] = [
-		{ value: 'do 1950'},
-		{ value: 'od 1950 do 1989'},
-		{ value: 'od 1990 do 2010'},
-		{ value: 'od 2010'}
+		{ value: 'do 1950' },
+		{ value: 'od 1950 do 1989' },
+		{ value: 'od 1990 do 2010' },
+		{ value: 'od 2010' },
 	];
 	public properties: IProperty[] = [
-		{ value: 'Kawalerka'},
-		{ value: 'Mieszkanie'},
-		{ value: 'Pokój'}
+		{ value: 'Kawalerka' },
+		{ value: 'Mieszkanie' },
+		{ value: 'Pokój' },
 	];
 }
-
-
