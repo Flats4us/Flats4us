3452ca28c419c28569e0c1fcf6ae702ce519ff24
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-04-30 00:01:26 +0200

    #35 Ankieta studenta

--- angular/src/app/settings/settings.module.ts
+++ angular/src/app/settings/settings.module.ts
@@ -9,29 +9,29 @@ import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { HttpClientModule } from "@angular/common/http";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 import { JsonFormComponent } from './components/json-form/json-form.component';
-import { MatButtonModule } from "@angular/material/button";
-import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
 	declarations: [StudentSurveyComponent, JsonFormComponent],
-  imports: [
-    CommonModule,
-    SettingsRoutingModule,
-    MatCardModule,
-    MatSliderModule,
-    MatSnackBarModule,
-    FormsModule,
-    MatListModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSlideToggleModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatCheckboxModule
-  ]
+	imports: [
+		CommonModule,
+		SettingsRoutingModule,
+		MatCardModule,
+		MatSliderModule,
+		MatSnackBarModule,
+		FormsModule,
+		MatListModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatSlideToggleModule,
+		HttpClientModule,
+		MatButtonModule,
+		MatCheckboxModule,
+	],
 })
 export class SettingsModule {}
