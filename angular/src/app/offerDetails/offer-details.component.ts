82d582848df9633ecec489bdbf23a0d9032ecba3
Jakub Gizewski <s22523@pjwstk.edu.pl>
2023-04-30 22:38:06 +0200

    wip

---
+++ angular/src/app/offerDetails/offer-details.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
+
+@Component({
+	selector: 'app-profile',
+	templateUrl: './offer-details.component.html',
+	styleUrls: ['./offer-details.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OfferDetailsComponent {
+	public id$: Observable<string>;
+
+	constructor(private route: ActivatedRoute) {
+		this.id$ = this.route.paramMap.pipe(
+			map((params) => params.get('id')),
+			filter(Boolean)
+		);
+	}
+}
