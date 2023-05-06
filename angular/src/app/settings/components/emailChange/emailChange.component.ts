67a52f29fbd3d2806d79bcd497089ded210272b5
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-05-06 17:08:31 +0200

    #35 Ankieta studenta

---
+++ angular/src/app/settings/components/emailChange/emailChange.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+	selector: 'app-email-change',
+	templateUrl: './emailChange.component.html',
+	styleUrls: ['./emailChange.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmailChangeComponent {
+	public hide = true;
+	public emailChangeForm: FormGroup;
+
+	constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+		this.emailChangeForm = this.fb.group({
+			email: ['s22900@pjwstk.edu.pl', [Validators.required, Validators.email]],
+			password: ['', Validators.required],
+		});
+	}
+
+	public onSubmit() {
+		this.snackBar.open('Pomyślnie zmieniono adres mailowy!', 'Zamknij', {
+			duration: 2000,
+		});
+	}
+}
