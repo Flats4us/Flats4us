ef9becf2906fcfc0e3392fb466eccadfb64f3ed9
Smurf228 <109930207+Smurf228@users.noreply.github.com>
2023-05-07 21:00:06 +0200

    #2ownerForm

--- angular/src/app/owner-form/owner-form-routing.module.ts
+++ angular/src/app/owner-form/owner-form-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { OwnerFormComponent } from './owner-form.component';
 
-const routes: Routes = [{ path: 'ownerForm', component: OwnerFormComponent }];
+const routes: Routes = [{ path: '', component: OwnerFormComponent }];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
