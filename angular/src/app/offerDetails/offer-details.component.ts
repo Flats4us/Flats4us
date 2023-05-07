df71b2fc28e20806052412924b6db5d7daee1979
giziuuu <kubagizewski@gmail.com>
2023-05-07 22:18:56 +0200

    wip

--- angular/src/app/offerDetails/offer-details.component.ts
+++ angular/src/app/offerDetails/offer-details.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, Observable } from 'rxjs';
+import { GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
 	selector: 'app-offer-details',
@@ -10,11 +11,33 @@ import { filter, map, Observable } from 'rxjs';
 })
 export class OfferDetailsComponent {
 	public id$: Observable<string>;
+	images: GalleryItem[] = [];
 
 	constructor(private route: ActivatedRoute) {
 		this.id$ = this.route.paramMap.pipe(
 			map((params) => params.get('id')),
 			filter(Boolean)
 		);
+	}
+
+	ngOnInit() {
+		this.images = [
+			new ImageItem({
+				src: '../../assets/offer-details/example1.jpg',
+				thumb: '../../assets/offer-details/example1.jpg',
+			}),
+			new ImageItem({
+				src: '../../assets/offer-details/example2.jpg',
+				thumb: '../../assets/offer-details/example2.jpg',
+			}),
+			new ImageItem({
+				src: '../../assets/offer-details/example3.jpg',
+				thumb: '../../assets/offer-details/example3.jpg',
+			}),
+			new ImageItem({
+				src: '../../assets/offer-details/example4.jpg',
+				thumb: '../../assets/offer-details/example4.jpg',
+			}),
+		];
 	}
 }
--- angular/src/app/offerDetails/offer-details.module.ts
+++ angular/src/app/offerDetails/offer-details.module.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { GalleryModule } from 'ng-gallery';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { OfferDetailsRoutingModule } from './offer-details-routing.module';
 import { OfferDetailsComponent } from './offer-details.component';
@@ -17,6 +19,8 @@ import { OfferDetailsComponent } from './offer-details.component';
 		MatButtonModule,
 		MatIconModule,
 		MatChipsModule,
+		GalleryModule,
+		MatSliderModule,
 	],
 	exports: [OfferDetailsComponent],
 })
