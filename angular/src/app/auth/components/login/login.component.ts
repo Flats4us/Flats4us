6799c05355db0edd1c03fe60da84b448902af93a
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2023-05-07 20:57:50 +0200

    Login component added (#14)

--- angular/src/app/auth/components/login/login.component.ts
+++ angular/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-login',
@@ -6,4 +8,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	styleUrls: ['./login.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+	public hide = true;
+	public loginForm: FormGroup;
+
+	constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+		this.loginForm = this.fb.group({
+			email: ['', Validators.required],
+			password: ['', Validators.required],
+		});
+	}
+
+	public onSubmit() {
+		this.snackBar.open('Zalogowano pomyślnie!', 'Zamknij', {
+			duration: 2000,
+		});
+	}
+}
