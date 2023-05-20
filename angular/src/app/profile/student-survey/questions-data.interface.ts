32fb38af8d4625323d3374271670e331fbe3b515
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-05-20 23:11:46 +0200

    #35-Ankieta-Studenta

--- angular/src/app/profile/student-survey/questions-data.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IQuestionsData {
-	questions: {
-		id: number;
-		title: string;
-		content: string;
-		typeName: string;
-		answers: string[];
-	}[];
-}
--- angular/src/app/profile/student-survey/student-survey.component.ts
+++ angular/src/app/profile/student-survey/student-survey.component.ts
@@ -4,10 +4,9 @@ import {
 	OnInit,
 	ChangeDetectorRef,
 } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { IQuestionsData } from './questions-data.interface';
 
 @Component({
 	selector: 'app-student-survey',
@@ -25,41 +24,35 @@ export class StudentSurveyComponent implements OnInit {
 		private snackBar: MatSnackBar,
 		private http: HttpClient,
 		private cdr: ChangeDetectorRef
-	) {
-		this.studentSurveyForm = this.formBuilder.group({
-			party: 0,
-			tidy: 0,
-			smoke: 0,
-			social: 0,
-			petOwner: 0,
-			veganPerson: 0,
-			maxNumberOfRoommates: ['', Validators.required],
-			minRoommateAge: ['', Validators.required],
-			maxRoommateAge: ['', Validators.required],
-			lookingForRoommates: [''],
-		});
-	}
+	) {}
 
-	public ngOnInit(): void {
-		this.http
-			.get<IQuestionsData>('../../assets/survey.json')
-			.subscribe((data) => {
-				this.questions = data.questions;
-				this.cdr.detectChanges();
-			});
+	public ngOnInit() {
+		this.http.get<[]>('../../assets/JSON.json').subscribe((data) => {
+			this.questions = data;
+			this.cdr.detectChanges();
+		});
+
+		//this.studentSurveyForm = this.generateSurveyForm();
 
 		this.studentSurveyForm
 			.get('lookingForRoommates')
 			?.valueChanges.subscribe((value) => (this.showLookingForRoommates = value));
 	}
 
-	public getQuestionById(id: number) {
-		return this.questions.find((question) => question.id === id).content;
-	}
-
 	public onSubmit() {
 		this.snackBar.open('Pomyślnie wypełniono ankietę!', 'Zamknij', {
 			duration: 2000,
 		});
+	}
+
+	private generateSurveyForm(): FormGroup {
+		const baseForm = this.formBuilder.group({});
+		while (this.questions.length == 0) {
+			/* empty */
+		}
+		this.questions.forEach((question) => {
+			baseForm.addControl(question.formControlName, new FormControl(''));
+		});
+		return baseForm;
 	}
 }
