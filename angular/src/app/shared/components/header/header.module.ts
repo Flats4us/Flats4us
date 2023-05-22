7938ab924ee647e1217e2371e9ce08f692a60bf0
Danil <109930207+Smurf228@users.noreply.github.com>
2023-05-22 18:08:26 +0200

    Navbar2.0 (#22)
    * Navbar
    ---------
    
    Co-authored-by: Dominik Dudzik <dominik.dudzik@fim.pl>
    Co-authored-by: giziuuu <kubagizewski@gmail.com>
    Co-authored-by: Matthew <68421340+mattonaly@users.noreply.github.com>
    Co-authored-by: mattonaly <mattioskar.mat@gmail.com>
    Co-authored-by: Jakub Giżewski <76437687+giziuuu@users.noreply.github.com>
    Co-authored-by: Konrad Sochacki <s23801@pjwstk.edu.pl>

--- angular/src/app/shared/components/header/header.module.ts
+++ angular/src/app/shared/components/header/header.module.ts
@@ -8,6 +8,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
 	declarations: [HeaderComponent],
@@ -19,6 +23,10 @@ import { MatListModule } from '@angular/material/list';
 		BrowserAnimationsModule,
 		MatSidenavModule,
 		MatListModule,
+		MatRadioModule,
+		MatSlideToggleModule,
+		MatMenuModule,
+		RouterLink,
 	],
 	exports: [HeaderComponent],
 })
