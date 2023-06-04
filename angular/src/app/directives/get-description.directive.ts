cb651bbdf14f91648c4bdd36a3ab92272c9b0fd6
Konrad Sochacki <101300283+s23801@users.noreply.github.com>
2023-06-04 18:36:35 +0200

    #23MainPagePoZmianach (#40)
    * PoprawionyRevert
    
    ---------
    
    Co-authored-by: Danil <109930207+Smurf228@users.noreply.github.com>
    Co-authored-by: Dominik Dudzik <dominik.dudzik@fim.pl>
    Co-authored-by: giziuuu <kubagizewski@gmail.com>
    Co-authored-by: Matthew <68421340+mattonaly@users.noreply.github.com>
    Co-authored-by: Jakub Giżewski <76437687+giziuuu@users.noreply.github.com>

---
+++ angular/src/app/directives/get-description.directive.ts
@@ -0,0 +1,37 @@
+import {
+	Directive,
+	ElementRef,
+	Input,
+	OnChanges,
+	SimpleChanges,
+} from '@angular/core';
+
+@Directive({
+	selector: '[appGetDescription]',
+})
+export class GetDescriptionDirective implements OnChanges {
+	@Input('appGetDescription')
+	public count = 0;
+
+	constructor(private el: ElementRef) {}
+
+	public ngOnChanges(changes: SimpleChanges) {
+		this.el.nativeElement.textContent = this.getDescription(
+			changes['count'].currentValue
+		);
+	}
+
+	public getDescription(numberOfRecords: number) {
+		switch (true) {
+			case numberOfRecords == 1:
+				return `${numberOfRecords} oferta`;
+			case (numberOfRecords > 1 && numberOfRecords <= 4) ||
+				(numberOfRecords > 20 &&
+					numberOfRecords % 10 > 1 &&
+					numberOfRecords % 10 <= 4):
+				return `${numberOfRecords} oferty`;
+			default:
+				return `${numberOfRecords} ofert`;
+		}
+	}
+}
