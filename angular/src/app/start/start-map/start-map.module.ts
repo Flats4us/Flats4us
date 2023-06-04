eba26e4ae89710f329053b309fee5bcb74f199c5
mattonaly <mattioskar.mat@gmail.com>
2023-06-04 19:45:07 +0200

    feat: address search and markers

--- angular/src/app/start/start-map/start-map.module.ts
+++ angular/src/app/start/start-map/start-map.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { StartMapComponent } from './start-map.component';
 
 @NgModule({
 	declarations: [StartMapComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class StartMapModule {}
