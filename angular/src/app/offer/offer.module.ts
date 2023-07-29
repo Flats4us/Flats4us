b8f02a79a42928118ea6ccbb49174f8cf59d57d0
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-07-29 11:50:56 +0200

    #44-fe-add-offer

--- angular/src/app/offer/offer.module.ts
+++ angular/src/app/offer/offer.module.ts
@@ -6,6 +6,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	exports: [AddOfferComponent],
@@ -17,6 +22,11 @@ import { MatInputModule } from '@angular/material/input';
 		MatFormFieldModule,
 		ReactiveFormsModule,
 		MatInputModule,
+		MatDatepickerModule,
+		MatNativeDateModule,
+		MatSelectModule,
+		MatButtonModule,
+		MatIconModule,
 	],
 })
 export class OfferModule {}
