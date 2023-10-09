8f329b4c0af26699fc567808fa79671954ca7512
Konrad Sochacki <s23801@pjwstk.edu.pl>
2023-10-09 17:00:25 +0200

    Poprawki

--- angular/src/app/add-real-estate/add-real-estate.component.ts
+++ angular/src/app/add-real-estate/add-real-estate.component.ts
@@ -6,9 +6,19 @@ import {
 	Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IGroup, INumeric, IRegionCity } from './models/add-real-estate.models';
+import { IGroup, IRegionCity } from './models/add-real-estate.models';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import {
+	citiesGroups,
+	districtGroups,
+	equipment,
+	numberOfFloors,
+	properties,
+	readCitiesForRegions,
+	regions,
+	yearOfBuilds,
+} from '../shared/models/realEstate';
 
 @Component({
 	selector: 'app-add-real-estate',
@@ -31,7 +41,15 @@ export class AddRealEstateComponent implements OnInit {
 	public completed = false;
 	public fileName = '';
 	public url: any;
-	public urls: any;
+	public urls: object[] = [];
+
+	public citiesGroups = citiesGroups;
+	public districtGroups = districtGroups;
+	public regions = regions;
+	public numberOfFloors = numberOfFloors;
+	public yearOfBuilds = yearOfBuilds;
+	public properties = properties;
+	public equipment = equipment;
 
 	constructor(
 		private formBuilder: FormBuilder,
@@ -75,26 +93,7 @@ export class AddRealEstateComponent implements OnInit {
 		this.addRealEstateForm3 = formBuilder.group({
 			photos: new FormControl(null, Validators.required),
 		});
-
-		this.http
-			.get('./assets/wojewodztwa_miasta.csv', { responseType: 'text' })
-			.subscribe((data) => {
-				const csvToRowArray = data.split('\n');
-				for (let index = 1; index < csvToRowArray.length - 1; index++) {
-					const row = csvToRowArray[index].split(';');
-					const lowerCaseRegion = row[2].trim().toLowerCase();
-					this.regionCityArray.push(<IRegionCity>{
-						region: lowerCaseRegion,
-						city: row[1],
-					});
-
-					this.citiesGroups
-						.find((group) => group.whole == lowerCaseRegion)
-						?.parts.push(row[1]);
-				}
-			});
-
-		this.urls = [];
+		readCitiesForRegions(this.http, this.regionCityArray, this.citiesGroups);
 	}
 
 	public filter = (opt: string[], value: string): string[] => {
@@ -123,10 +122,18 @@ export class AddRealEstateComponent implements OnInit {
 		this.router.navigate(['start/map']);
 	}
 
-	public isDone() {
-		if (this.addRealEstateForm1.valid && this.addRealEstateForm2.valid) {
-			this.router.navigate(['/']);
+	public saveRealEstate() {
+		if (
+			this.addRealEstateForm1.valid &&
+			this.addRealEstateForm2.valid &&
+			this.addRealEstateForm3.valid
+		) {
+			this.completed = true;
 		}
+	}
+
+	public addOffer() {
+		this.router.navigate(['/']);
 	}
 
 	public ngOnInit() {
@@ -186,199 +193,7 @@ export class AddRealEstateComponent implements OnInit {
 	public formReset() {
 		this.addRealEstateForm1.reset();
 		this.addRealEstateForm2.reset();
+		this.addRealEstateForm3.reset();
 		this.urls = [];
 	}
-
-	public citiesGroups: IGroup[] = [
-		{
-			whole: 'dolnośląskie',
-			parts: [],
-		},
-		{
-			whole: 'kujawsko-pomorskie',
-			parts: [],
-		},
-		{
-			whole: 'lubelskie',
-			parts: [],
-		},
-		{
-			whole: 'lubuskie',
-			parts: [],
-		},
-		{
-			whole: 'łódzkie',
-			parts: [],
-		},
-		{
-			whole: 'małopolskie',
-			parts: [],
-		},
-		{
-			whole: 'mazowieckie',
-			parts: [],
-		},
-		{
-			whole: 'opolskie',
-			parts: [],
-		},
-		{
-			whole: 'podkarpackie',
-			parts: [],
-		},
-		{
-			whole: 'podlaskie',
-			parts: [],
-		},
-		{
-			whole: 'pomorskie',
-			parts: [],
-		},
-		{
-			whole: 'śląskie',
-			parts: [],
-		},
-		{
-			whole: 'świętokrzyskie',
-			parts: [],
-		},
-		{
-			whole: 'warmińsko-mazurskie',
-			parts: [],
-		},
-		{
-			whole: 'wielkopolskie',
-			parts: [],
-		},
-		{
-			whole: 'zachodniopomorskie',
-			parts: [],
-		},
-	];
-
-	public districtGroups: IGroup[] = [
-		{
-			whole: 'Warszawa',
-			parts: [
-				'Bemowo',
-				'Białołęka',
-				'Bielany',
-				'Mokotów',
-				'Ochota',
-				'Praga-Południe',
-				'Praga-Północ',
-				'Rembertów',
-				'Śródmieście',
-				'Targówek',
-				'Ursus',
-				'Ursynów',
-				'Wawer',
-				'Wesoła',
-				'Wilanów',
-				'Włochy',
-				'Wola',
-				'Żoliborz',
-			],
-		},
-		{
-			whole: 'Gdańsk',
-			parts: [
-				'Aniołki',
-				'Brętowo',
-				'Brzeźno',
-				'Chełm',
-				'Jasień',
-				'Kokoszki',
-				'Krakowiec-Górki Zachodnie',
-				'Letnica',
-				'Matarnia',
-				'Młyniska',
-				'Nowy Port',
-				'Oliwa',
-				'Olszynka',
-				'Orunia-Św. Wojciech-Lipce',
-				'Osowa',
-				'Piecki-Migowo',
-				'Przeróbka',
-				'Przymorze Małe',
-				'Przymorze Wielkie',
-				'Rudniki',
-				'Siedlce',
-				'Stogi',
-				'Strzyża',
-				'Suchanino',
-				'Śródmieście',
-				'Ujeścisko-Łostowice',
-				'VII Dwór',
-				'Wrzeszcz Dolny',
-				'Wrzeszcz Górny',
-				'Wyspa Sobieszewska',
-				'Wzgórze Mickiewicza',
-				'Zaspa-Młyniec',
-				'Zaspa-Rozstaje',
-				'Żabianka-Wejhera-Jelitkowo-Tysiąclecia',
-			],
-		},
-		{
-			whole: 'Kraków',
-			parts: [
-				'Stare Miasto',
-				'Grzegórzki',
-				'Prądnik Czerwony',
-				'Prądnik Biały',
-				'Krowodrza',
-				'Bronowice',
-				'Zawierzyniec',
-				'Dębniki',
-				'Łagiewniki-Borek Fałęcki',
-				'Swoszowice',
-				'Podgórze Duchackie',
-				'Bieżanów-Prokocim',
-				'Podgórze',
-				'Czyżyny',
-				'Mistrzejowice',
-				'Bieńczyce',
-				'Wzgórze Krzesławickie',
-				'Nowa Huta',
-			],
-		},
-	];
-
-	public regions: string[] = [
-		'dolnośląskie',
-		'kujawsko-pomorskie',
-		'lubelskie',
-		'lubuskie',
-		'łódzkie',
-		'małopolskie',
-		'mazowieckie',
-		'opolskie',
-		'podkarpackie',
-		'podlaskie',
-		'pomorskie',
-		'śląskie',
-		'świętokrzyskie',
-		'warmińsko-mazurskie',
-		'wielkopolskie',
-		'zachodniopomorskie',
-	];
-	public numberOfFloors: INumeric[] = [
-		{ value: 1, viewValue: '1' },
-		{ value: 2, viewValue: '2' },
-		{ value: 3, viewValue: '3' },
-		{ value: 4, viewValue: '4' },
-		{ value: 5, viewValue: '5' },
-		{ value: 10, viewValue: '10' },
-		{ value: 20, viewValue: '20' },
-		{ value: 50, viewValue: '40' },
-		{ value: 100, viewValue: '80' },
-	];
-	public yearOfBuilds: string[] = [
-		'do 1950',
-		'od 1950 do 1989',
-		'od 1990 do 2010',
-		'od 2010',
-	];
-	public properties: string[] = ['Dom', 'Kawalerka', 'Mieszkanie', 'Pokój'];
-	public equipment: string[] = ['Winda', 'Pralka', 'Zmywarka'];
 }
