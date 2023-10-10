2521d1d7207a35f3911fb1aa4be7cea9c2d0363a
Matthew <68421340+mattonaly@users.noreply.github.com>
2023-10-11 00:30:08 +0200

    feat: add messages module (#76)

---
+++ angular/src/app/messages/components/messages-conversation/messages-conversation.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { MessagesConversationComponent } from './messages-conversation.component';
+
+@NgModule({
+	declarations: [MessagesConversationComponent],
+	imports: [CommonModule, MatInputModule, MatIconModule, ReactiveFormsModule],
+})
+export class MessagesConversationModule {}
