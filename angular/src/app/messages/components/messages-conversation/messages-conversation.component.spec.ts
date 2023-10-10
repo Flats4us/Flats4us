dd437819563ade5168aac5db61c09a179d91dec7
mattonaly <mattioskar.mat@gmail.com>
2023-10-11 00:28:28 +0200

    feat: add messages module

---
+++ angular/src/app/messages/components/messages-conversation/messages-conversation.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessagesConversationComponent } from './messages-conversation.component';
+
+describe('MessagesConversationComponent', () => {
+	let component: MessagesConversationComponent;
+	let fixture: ComponentFixture<MessagesConversationComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [MessagesConversationComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(MessagesConversationComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
