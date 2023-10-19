172c44df23c840c27f54c35172f3b87863d9cb31
Danil <109930207+Smurf228@users.noreply.github.com>
2023-10-19 18:10:08 +0200

    rebase main

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import localePl from '@angular/common/locales/pl';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+=======
+>>>>>>> be7a9e7 (rebase main)
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterModule } from '@shared/components/footer/footer.module';
@@ -20,6 +23,7 @@ import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 
 registerLocaleData(localePl);
+import { FindRoommateModule } from './find-roommate/find-roommate.module';
 
 @NgModule({
 	declarations: [AppComponent, NotFoundComponent],
@@ -37,7 +41,8 @@ registerLocaleData(localePl);
 		MatMenuModule,
 		MatNativeDateModule,
 		MatCardModule,
-	],
+		FindRoommateModule,
+  ],
 	providers: [
 		{ provide: LOCALE_ID, useValue: 'pl' },
 		{
