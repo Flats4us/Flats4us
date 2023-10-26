4f6cf4252d60b4be42b4fae6289ea9e535d224a7
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-10-26 16:23:27 +0200

    89-fe-start-dispute

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FindRoommateModule } from './find-roommate/find-roommate.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
 	declarations: [AppComponent, NotFoundComponent],
@@ -30,6 +31,7 @@ import { MatMenuModule } from '@angular/material/menu';
 		MatButtonModule,
 		MatIconModule,
 		MatMenuModule,
+		MatDialogModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
--- angular/src/app/shared/components/header/header.component.ts
+++ angular/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,14 @@
+// eslint-disable-next-line max-classes-per-file
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+	MatDialog,
+	MatDialogModule,
+	MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-header',
@@ -7,9 +17,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+	constructor(public dialog: MatDialog) {}
+
 	public isUserLoggedIn = true;
 	public isUserLoggedInAsStudent = true;
 
-	public id = '';
 	public showSidenav = false;
+
+	public animal = 'Dog';
+	public name = 'Kuba';
+
+	public openDialog(): void {
+		const dialogRef = this.dialog.open(StartDisputeComponent, {
+			width: '600px',
+			data: { name: this.name, animal: this.animal },
+		});
+	}
+}
+
+@Component({
+	selector: 'app-start-dispute-dialog',
+	templateUrl: 'start-dispute-dialog.html',
+	standalone: true,
+	imports: [
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
+		FormsModule,
+		MatButtonModule,
+	],
+})
+export class StartDisputeComponent {
+	constructor(public dialogRef: MatDialogRef<StartDisputeComponent>) {}
 }
