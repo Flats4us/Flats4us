8bfa5de0cbd64779d871498fd7367e68864f4ad7
Danil <109930207+Smurf228@users.noreply.github.com>
2023-10-26 16:45:25 +0200

    81-fe-add-opinion

--- angular/src/app/add-opinion/add-opinion.component.ts
+++ angular/src/app/add-opinion/add-opinion.component.ts
@@ -8,8 +8,37 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class AddOpinionComponent {
 	public commentText = '';
+	public selectedTags: string[] = [];
+	public checkedTags: Set<string> = new Set();
+	public comments: any[] = [];
+	public toggleCheckbox(tag: string): void {
+		if (this.isChecked(tag)) {
+			this.checkedTags.delete(tag);
+		} else {
+			this.checkedTags.add(tag);
+		}
+	}
 
-	public addTagToComment(tag: string) {
-		this.commentText += `#${tag} `;
+	public isChecked(tag: string): boolean {
+		return this.checkedTags.has(tag);
+	}
+
+	constructor() {
+		this.comments = [
+			{ text: ' 1', tags: ['Tag1', 'Tag2'] },
+			{ text: ' 2', tags: ['Tag3'] },
+		];
+	}
+
+	public addComment() {
+		const newComment = {
+			text: this.commentText,
+			tags: this.selectedTags,
+		};
+		this.comments.push(newComment);
+
+		// Очистите текст комментария и сбросьте выбранные теги
+		this.commentText = '';
+		this.selectedTags = [];
 	}
 }
