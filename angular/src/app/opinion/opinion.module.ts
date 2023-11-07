3e5fe71806a63a0a6f887319a4f5c5fd63706b87
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-11-07 21:45:44 +0100

    Star rating and formControls added

--- angular/src/app/opinion/opinion.module.ts
+++ angular/src/app/opinion/opinion.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { OpinionRoutingModule } from './opinion-routing.module';
 import { AddOpinionComponent } from './add-opinion/add-opinion.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
 	exports: [AddOpinionComponent],
@@ -24,6 +25,9 @@ import { MatListModule } from '@angular/material/list';
 		MatCheckboxModule,
 		MatInputModule,
 		MatListModule,
+		NgOptimizedImage,
+		ReactiveFormsModule,
+		MatSnackBarModule,
 	],
 })
 export class OpinionModule {}
