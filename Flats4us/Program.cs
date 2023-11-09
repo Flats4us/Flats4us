6f4cb80b685f8c583a18fa3f9c2cdf36cb19eb99
Jakub Giżewski <s22523@pjwstk.edu.pl>
2023-11-09 18:43:14 +0100

    fix: Program.cs

--- Flats4us/Program.cs
+++ Flats4us/Program.cs
@@ -1,12 +1,9 @@
 using Flats4us.Entities;
 using Flats4us.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Flats4us.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,51 +18,11 @@ builder.Services.AddScoped<IPropertyService, PropertyService>();
 builder.Services.AddScoped<IOfferService, OfferService>();
 builder.Services.AddScoped<IEquipmentService, EquipmentService>();
 
-//builder.Services.AddScoped<ISurveyOwnerOfferService, SurveyOwnerOfferService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-            .GetBytes(builder.Configuration.GetSection("Jwt:Key").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
-
-
-//builder.Services.AddScoped<IUserService, StudentService>();
-//builder.Services.AddScoped<IUserService, OwnerService>();
-builder.Services.AddScoped<StudentService>();
-builder.Services.AddScoped<OwnerService>();
-
-builder.Services.AddScoped<Func<string, IUserService>>(serviceProvider => key =>
-{
-    switch (key)
-    {
-        case "Student":
-            return serviceProvider.GetService<StudentService>();
-        case "Owner":
-            return serviceProvider.GetService<OwnerService>();
-        default:
-            throw new KeyNotFoundException(); 
-    }
-});
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminOrTenantOnly", policy =>
-    {
-        policy.RequireRole("Admin", "Tenant");
-    });
-}); 
 
 Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
@@ -86,43 +43,32 @@ builder.Services.AddCors(c =>
                                                     .AllowAnyMethod());
 });
 
-using var scope = builder.Services.BuildServiceProvider().CreateScope();
-var dbContext = scope.ServiceProvider.GetRequiredService<Flats4usContext>();
-dbContext.Database.Migrate();
-
 var app = builder.Build();
-app.UseStaticFiles();   // for swagger
 
-//using (var scope = app.Services.CreateScope())
-//{
-  //  var services = scope.ServiceProvider;
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
     // Get the database context
-    //var dbContext = services.GetRequiredService<Flats4usContext>();
+    var dbContext = services.GetRequiredService<Flats4usContext>();
 
     // Ensure the database is created and tables are created if they don't exist
-    //if (dbContext.Database.EnsureCreated())
-    //{
-      //  DataSeeder.SeedData(dbContext);
-    //}
-//}
+    if (dbContext.Database.EnsureCreated())
+    {
+        DataSeeder.SeedData(dbContext);
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
 }
-
-
-
 
 app.UseCors(options => options.AllowAnyOrigin());
 
-//app.UseHttpsRedirection();
-
-app.UseAuthentication();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
