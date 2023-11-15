b88d638ae7294dd8d145e41a15c31ed85ec91c15
s20960 <s20960@pjwstk.edu.pl>
2023-11-15 21:51:27 +0100

    mała zmiana

--- Flats4us/Entities/Dto/ArgumentInterventionDto.cs
+++ Flats4us/Entities/Dto/ArgumentInterventionDto.cs
@@ -1,6 +1,16 @@
-﻿namespace Flats4us.Entities.Dto
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flats4us.Entities.Dto
 {
     public class ArgumentInterventionDto
     {
+        public int ArgumentInterventionId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Justification { get; set; }
     }
 }
--- Flats4us/Services/Interfaces/IArgumentInterventionService.cs
+++ Flats4us/Services/Interfaces/IArgumentInterventionService.cs
@@ -1,6 +1,13 @@
-﻿namespace Flats4us.Services.Interfaces
+﻿using Flats4us.Entities;
+using Flats4us.Entities.Dto;
+
+
+namespace Flats4us.Services.Interfaces
 {
     public interface IArgumentInterventionService
     {
+        public Task<List<ArgumentIntervention>> GetAllAsync();
+        public Task<ArgumentIntervention> GetById(int id);
+        public Task AddArgumentInterventionAsync(ArgumentInterventionDto input);
     }
 }
