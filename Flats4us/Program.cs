9bea69f528f07b22424896f92c3b74e6dd56da51
giziuuu <kubagizewski@gmail.com>
2023-11-20 02:27:53 +0100

    wip

--- Flats4us/Program.cs
+++ Flats4us/Program.cs
@@ -148,8 +148,7 @@ app.UseAuthorization();
 app.UseHangfireDashboard();
 
 var backgroundJobService = app.Services.GetRequiredService<IBackgroundJobService>();
-
-HangfireJobs.ConfigureJobs(backgroundJobService);
+HangfireSetup.ConfigureJobs(backgroundJobService);
 
 app.MapControllers();
 
