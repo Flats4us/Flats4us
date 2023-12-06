b5933b35f34c372b4fd9bf7063191d3f42d30120
mattonaly <mattioskar.mat@gmail.com>
2023-12-06 21:31:08 +0100

    feat: fe docker image + base component + auth login

---
+++ angular/src/app/shared/services/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+	CanActivate,
+	ActivatedRouteSnapshot,
+	RouterStateSnapshot,
+	Router,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+	constructor(private authService: AuthService, private router: Router) {}
+
+	public canActivate(
+		next: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	): boolean {
+		return this.checkAuthentication(state.url);
+	}
+
+	public checkAuthentication(url: string): boolean {
+		if (this.authService.isLoggedIn()) {
+			return true;
+		}
+
+		this.router.navigate(['/auth/login'], { queryParams: { returnUrl: url } });
+		return false;
+	}
+}
