c43c1d1c2e73b1c7567d231f239d889a30617701
s20960 <s20960@pjwstk.edu.pl>
2023-12-07 16:54:44 +0100

    wip

---
+++ Flats4us/Services/PaymentService.cs
@@ -0,0 +1,36 @@
+﻿using Flats4us.Entities;
+using Flats4us.Helpers.Enums;
+using Flats4us.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flats4us.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public readonly Flats4usContext _context;
+
+        public PaymentService(Flats4usContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Payment> GetPaymentByRentId(int id)
+        {
+            return await _context.Payments.FirstAsync(x => x.RentId == id);
+        }
+
+        public async Task EditStatusPaymentAsync(int id, PaymentStatus status)
+        {
+            var payment = await _context.Payments.FirstAsync(x => x.PaymentId == id);
+            if (payment == null)
+                return;
+
+            payment.PaymentStatus = status;
+
+            //await _context.Payments.UpdateAsync(payment);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
