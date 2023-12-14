69726ff611b107fe90a9ec3b39683aad2ee5a51d
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2023-12-14 17:05:52 +0100

    Removed unused import

--- angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
+++ angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { IStudent } from '../../models/roommate-candidate.models';
