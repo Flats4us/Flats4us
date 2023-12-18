5f4eac78563669747ff03257e454d8cac35d3d6e
s20960 <s20960@pjwstk.edu.pl>
2023-12-18 22:18:47 +0100

    wstawione klasy

---
+++ Flats4us/Entities/Grindr.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flats4us.Entities
+{
+    public class Grindr
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GrindrId { get; set; }
+
+
+        public bool? isStudent1Interested { get; set; }
+
+
+        public bool? isStudent2Interested { get; set; }
+
+        [Required]
+        public int Student1Id { get; set; }
+
+        [Required]
+        public int Student2Id { get; set; }
+
+
+        public virtual Student Student1 { get; set; }
+        public virtual Student Student2 { get; set; }
+    }
+}
