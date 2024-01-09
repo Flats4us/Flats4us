b771ee6c39504b52c7fc1874082844c0b0d0bf2c
s20960 <s20960@pjwstk.edu.pl>
2024-01-09 14:48:48 +0100

    wip

--- Flats4us/Entities/Dto/TechnicalProblemDto.cs
+++ Flats4us/Entities/Dto/TechnicalProblemDto.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Flats4us.Helpers.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Flats4us.Entities.Dto
 {
     public class TechnicalProblemDto
     {
         [Required]
-        public TechnicalProblem Problem { get; set; }
+        public TechnicalProblemEnum Kind { get; set; }
 
         [Required]
         public string Description { get; set; }
