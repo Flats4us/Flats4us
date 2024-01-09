c57581d873f0ecfe333786d7561ade16d568d31a
s20960 <s20960@pjwstk.edu.pl>
2024-01-09 14:48:48 +0100

    wip

--- Flats4us/Entities/TechnicalProblem.cs
+++ Flats4us/Entities/TechnicalProblem.cs
@@ -1,6 +1,26 @@
-﻿namespace Flats4us.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Flats4us.Helpers.Enums;
+
+namespace Flats4us.Entities
 {
     public class TechnicalProblem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TechnicalProblemId { get; set; }
+
+        [Required]
+        public TechnicalProblemEnum Kind { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
     }
 }
