24b0d23c20d9077c5afb5e1c9042dc9e1d848db9
Dominik Dudzik <dominik.dudzik@fim.pl>
2024-01-11 16:43:17 +0100

    .

---
+++ Flats4us/Entities/Dto/EditStudentSensitiveDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Flats4us.Entities.Dto
+{
+    public class EditStudentSensitiveDto
+    {
+        public DateTime? BirthDate { get; set; }
+        public string? StudentNumber { get; set; }
+        public string? University { get; set; }
+
+    }
+}
---
+++ Flats4us/Entities/Dto/EditUserGeneral.cs
@@ -0,0 +1,10 @@
+﻿namespace Flats4us.Entities.Dto
+{
+    public class EditUserGeneral
+    {
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
