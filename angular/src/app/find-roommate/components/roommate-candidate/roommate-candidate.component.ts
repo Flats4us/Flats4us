e946c5e8a54bd5168182938aaf0f61e150874443
Kuba Sobolewski <s22900@pjwstk.edu.pl>
2024-01-18 15:14:44 +0100

    Deleted scale, added takeuntil

--- angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
+++ angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
@@ -3,6 +3,7 @@ import { map, Observable } from 'rxjs';
 import { IStudent } from '../../models/roommate-candidate.models';
 import { FindRoommateService } from '../../services/find-roommate.service';
 import { environment } from '../../../../environments/environment.prod';
+import { BaseComponent } from '@shared/components/base/base.component';
 
 @Component({
 	selector: 'app-roommate-candidate',
@@ -10,7 +11,7 @@ import { environment } from '../../../../environments/environment.prod';
 	styleUrls: ['./roommate-candidate.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoommateCandidateComponent {
+export class RoommateCandidateComponent extends BaseComponent {
 	protected iterator = 0;
 
 	public students$: Observable<IStudent[]> = this.service.getStudents();
@@ -21,7 +22,9 @@ export class RoommateCandidateComponent {
 
 	protected baseUrl = environment.apiUrl.replace('/api', '');
 
-	constructor(private service: FindRoommateService) {}
+	constructor(private service: FindRoommateService) {
+		super();
+	}
 
 	private updateStudent() {
 		this.iterator += 1;
@@ -34,11 +37,17 @@ export class RoommateCandidateComponent {
 
 	public accept(id: number) {
 		this.updateStudent();
-		this.service.accept(id, { decision: true }).subscribe();
+		this.service
+			.accept(id, { decision: true })
+			.pipe(this.untilDestroyed())
+			.subscribe();
 	}
 
 	public reject(id: number) {
 		this.updateStudent();
-		this.service.accept(id, { decision: false }).subscribe();
+		this.service
+			.accept(id, { decision: false })
+			.pipe(this.untilDestroyed())
+			.subscribe();
 	}
 }
