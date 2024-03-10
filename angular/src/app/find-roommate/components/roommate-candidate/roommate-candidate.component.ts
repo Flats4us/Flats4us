ce526e340d4d419e8c9fcd7cb68ee45edad42374
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2024-03-10 15:27:46 +0100

    JSON do ekranu szukania współlokatora (#141)
    * Roommate candidate data is now loaded from JSON file
    
    * Removed unused import
    
    * getStudent method moved to service, matchLevel deleted
    
    * Backend integration initial commit
    
    * Request body is now in JSON format
    
    * Deleted scale, added takeuntil

--- angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
+++ angular/src/app/find-roommate/components/roommate-candidate/roommate-candidate.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { IStudent } from '../../models/roommate-candidate.models';
+import { FindRoommateService } from '../../services/find-roommate.service';
+import { environment } from '../../../../environments/environment.prod';
+import { BaseComponent } from '@shared/components/base/base.component';
 
 @Component({
 	selector: 'app-roommate-candidate',
@@ -6,4 +11,43 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	styleUrls: ['./roommate-candidate.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoommateCandidateComponent {}
+export class RoommateCandidateComponent extends BaseComponent {
+	protected iterator = 0;
+
+	public students$: Observable<IStudent[]> = this.service.getStudents();
+
+	public student$: Observable<IStudent> = this.students$.pipe(
+		map(students => students[this.iterator])
+	);
+
+	protected baseUrl = environment.apiUrl.replace('/api', '');
+
+	constructor(private service: FindRoommateService) {
+		super();
+	}
+
+	private updateStudent() {
+		this.iterator += 1;
+		if (this.iterator >= 5) {
+			this.students$ = this.service.getStudents();
+			this.iterator = 0;
+		}
+		this.student$ = this.students$.pipe(map(students => students[this.iterator]));
+	}
+
+	public accept(id: number) {
+		this.updateStudent();
+		this.service
+			.accept(id, { decision: true })
+			.pipe(this.untilDestroyed())
+			.subscribe();
+	}
+
+	public reject(id: number) {
+		this.updateStudent();
+		this.service
+			.accept(id, { decision: false })
+			.pipe(this.untilDestroyed())
+			.subscribe();
+	}
+}
