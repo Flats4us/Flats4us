14e6f96525fe5edef48d2feb6605ec03d665200c
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-03-22 16:18:44 +0100

    Prettier-2

--- angular/src/app/rents/components/dialog/rents-cancel-dialog/rents-cancel-dialog.component.spec.ts
+++ angular/src/app/rents/components/dialog/rents-cancel-dialog/rents-cancel-dialog.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RentsCancelDialogComponent } from './rents-cancel-dialog.component';
 
 describe('RentsCancelDialogComponent', () => {
-  let component: RentsCancelDialogComponent;
-  let fixture: ComponentFixture<RentsCancelDialogComponent>;
+	let component: RentsCancelDialogComponent;
+	let fixture: ComponentFixture<RentsCancelDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ RentsCancelDialogComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RentsCancelDialogComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(RentsCancelDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(RentsCancelDialogComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
