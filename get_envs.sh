ef4f1f96fec758c76163f32d79cf42f0a792ae25
mattonaly <mattioskar.mat@gmail.com>
2024-04-08 18:29:05 +0200

    fix: update env path

--- get_envs.sh
+++ get_envs.sh
@@ -5,5 +5,5 @@ cd /home/Flats4us
 COMMIT_HASH=$(git rev-parse HEAD)
 COMMIT_DATE=$(git log -1 --format=%cd --date=format:"%Y-%m-%d %H:%M:%S")
 
-sed -i "s/##COMMIT_HASH##/$COMMIT_HASH/g" src/environments/environment.prod.ts
-sed -i "s/##COMMIT_DATE##/$COMMIT_DATE/g" src/environments/environment.prod.ts
+sed -i "s/##COMMIT_HASH##/$COMMIT_HASH/g" angular/src/environments/environment.prod.ts
+sed -i "s/##COMMIT_DATE##/$COMMIT_DATE/g" angular/src/environments/environment.prod.ts
