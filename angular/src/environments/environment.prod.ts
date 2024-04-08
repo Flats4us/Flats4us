6e8e5128ab6c99931bd5d844ececb1c79d25b98f
mattonaly <mattioskar.mat@gmail.com>
2024-04-08 18:28:04 +0200

    feat: add version to fe

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
 	production: false,
 	apiUrl: 'http://172.21.40.120:5166/api',
+	commitHash: '##COMMIT_HASH##',
+	commitDate: '##COMMIT_DATE##',
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
 	production: false,
 	apiUrl: '/api',
+	commitHash: '##COMMIT_HASH##',
+	commitDate: '##COMMIT_DATE##',
 };
