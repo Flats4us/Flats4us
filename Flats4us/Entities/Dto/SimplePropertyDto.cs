1b64f09865261f8d2a5150100f5f61d19658fe0b
giziuuu <kubagizewski@gmail.com>
2024-04-08 13:06:37 +0200

    wip

---
+++ Flats4us/Entities/Dto/SimplePropertyDto.cs
@@ -0,0 +1,19 @@
+﻿using Flats4us.Helpers.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flats4us.Entities.Dto
+{
+    public class SimplePropertyDto
+    {
+        [Required]
+        public double GeoLat { get; set; }
+
+        [Required]
+        public double GeoLon { get; set; }
+
+        [Required]
+        public PropertyType PropertyType { get; set; }
+
+        public ICollection<EquipmentDto> Equipments { get; set; }
+    }
+}
