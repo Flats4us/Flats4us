ac0de5d01d145ae883fbf9bb34718f3153a1275b
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-04-15 17:19:48 +0200

    ZmianyStartSite+Mapa+IkonyWyposazenia

--- angular/src/app/start/services/start.service.ts
+++ angular/src/app/start/services/start.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { IFilteredOffers, ISortOption } from '../models/start-site.models';
+import {
+	IFilteredMapOffers,
+	IFilteredOffers,
+	ISendMapOffers,
+	ISortOption,
+} from '../models/start-site.models';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -33,9 +38,6 @@ export class StartService {
 		},
 	];
 
-	public pageSize = 6;
-	public pageIndex = 0;
-
 	protected apiRoute = `${environment.apiUrl}/offers`;
 
 	public mapOffersForm: FormGroup = new FormGroup({
@@ -52,15 +54,14 @@ export class StartService {
 		rooms: new FormControl(null),
 		floors: new FormControl(null),
 		equipment: new FormControl([]),
-		sorting: new FormControl(this.sortByOptions[0]),
-		pageIndex: new FormControl(this.pageIndex),
-		pageSize: new FormControl(this.pageSize),
 	});
 
 	constructor(private httpClient: HttpClient) {}
 
 	public getFilteredOffers(
-		filteredOptions: IFilteredOffers
+		filteredOptions: IFilteredOffers,
+		index: number,
+		size: number
 	): Observable<ISendOffers> {
 		const properties = filteredOptions.property.join(', ').split(', ');
 		const years = filteredOptions.year.join(', ').split(', ');
@@ -112,9 +113,11 @@ export class StartService {
 				filteredOptions.regionsGroup.charAt(0).toUpperCase() +
 					filteredOptions.regionsGroup.slice(1)
 			)
-			.append('city', filteredOptions.citiesGroup)
-			.append('pageNumber', filteredOptions.pageIndex + 1)
-			.append('pageSize', filteredOptions.pageSize);
+			.append('pageNumber', index + 1)
+			.append('pageSize', size);
+		if (filteredOptions.citiesGroup) {
+			queryParams = queryParams.append('city', filteredOptions.citiesGroup);
+		}
 		if (filteredOptions.distance) {
 			queryParams = queryParams.append('distance', filteredOptions.distance);
 		}
@@ -154,6 +157,102 @@ export class StartService {
 			queryParams = queryParams.append('sorting', filteredOptions.sorting.type);
 		}
 		return this.httpClient.get<ISendOffers>(this.apiRoute, {
+			params: queryParams,
+		});
+	}
+
+	public getFilteredMapOffers(
+		filteredOptions: IFilteredMapOffers
+	): Observable<ISendMapOffers> {
+		const properties = filteredOptions.property.join(', ').split(', ');
+		const years = filteredOptions.year.join(', ').split(', ');
+		const minVal = Math.min(...years.map(year => parseInt(year)));
+		const maxVal = Math.max(...years.map(year => parseInt(year)));
+		let minYear = 0;
+		let maxYear = 0;
+		switch (minVal) {
+			case 1: {
+				minYear = 0;
+				break;
+			}
+			case 2: {
+				minYear = 1950;
+				break;
+			}
+			case 3: {
+				minYear = 1990;
+				break;
+			}
+			case 4: {
+				minYear = 2010;
+				break;
+			}
+		}
+		switch (maxVal) {
+			case 1: {
+				maxYear = 1950;
+				break;
+			}
+			case 2: {
+				maxYear = 1990;
+				break;
+			}
+			case 3: {
+				maxYear = 2010;
+				break;
+			}
+			case 4: {
+				maxYear = new Date().getFullYear();
+				break;
+			}
+		}
+		let queryParams = new HttpParams({
+			fromObject: { Equipment: filteredOptions.equipment },
+		}).append(
+			'province',
+			filteredOptions.regionsGroup.charAt(0).toUpperCase() +
+				filteredOptions.regionsGroup.slice(1)
+		);
+		if (filteredOptions.citiesGroup) {
+			queryParams = queryParams.append('city', filteredOptions.citiesGroup);
+		}
+		if (filteredOptions.distance) {
+			queryParams = queryParams.append('distance', filteredOptions.distance);
+		}
+		for (let i = 0; i < 3; i++) {
+			if (properties[i]) {
+				queryParams = queryParams.append('propertyTypes', properties[i]);
+			}
+		}
+		if (filteredOptions.minPrice) {
+			queryParams = queryParams.append('minPrice', filteredOptions.minPrice);
+		}
+		if (filteredOptions.maxPrice) {
+			queryParams = queryParams.append('maxPrice', filteredOptions.maxPrice);
+		}
+		if (filteredOptions.districtsGroup) {
+			queryParams = queryParams.append('district', filteredOptions.districtsGroup);
+		}
+		if (filteredOptions.minArea) {
+			queryParams = queryParams.append('minArea', filteredOptions.minArea);
+		}
+		if (filteredOptions.maxArea) {
+			queryParams = queryParams.append('maxArea', filteredOptions.maxArea);
+		}
+		if (minYear) {
+			queryParams = queryParams.append('minYear', minYear);
+		}
+		if (maxYear) {
+			queryParams = queryParams.append('maxYear', maxYear);
+		}
+		if (filteredOptions.rooms) {
+			queryParams = queryParams.append('minNumberOfRooms', filteredOptions.rooms);
+		}
+		if (filteredOptions.floors) {
+			queryParams = queryParams.append('floor', filteredOptions.floors);
+		}
+		const url = `${this.apiRoute}/map`;
+		return this.httpClient.get<ISendMapOffers>(url, {
 			params: queryParams,
 		});
 	}
