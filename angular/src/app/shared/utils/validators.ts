8f134e23674bef890a96c37ff6bbf401c1814616
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-04-29 17:49:14 +0200

    Poprawki, walidacja e-mail

--- angular/src/app/shared/utils/validators.ts
+++ angular/src/app/shared/utils/validators.ts
@@ -1,5 +1,4 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { UserService } from '@shared/services/user.service';
 
 export function validityAgeValidator(): ValidatorFn {
 	return (control: AbstractControl): ValidationErrors | null => {
