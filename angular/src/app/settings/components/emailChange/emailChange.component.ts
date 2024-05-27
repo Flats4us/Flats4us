0162354aed83dea100fbeb0564e863153a565538
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2024-05-27 11:34:32 +0200

    300-fe-email-change-integration (#301)
    * 300-fe-email-change-integration
    
    * prettier
    
    * changeEmail method simplification

--- angular/src/app/settings/components/emailChange/emailChange.component.ts
+++ angular/src/app/settings/components/emailChange/emailChange.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { BaseComponent } from '@shared/components/base/base.component';
+import { UserService } from '@shared/services/user.service';
 
 @Component({
 	selector: 'app-email-change',
@@ -8,20 +10,34 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 	styleUrls: ['./emailChange.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EmailChangeComponent {
+export class EmailChangeComponent extends BaseComponent {
 	public hide = true;
 	public emailChangeForm: FormGroup;
 
-	constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+	constructor(
+		private fb: FormBuilder,
+		private snackBar: MatSnackBar,
+		private service: UserService
+	) {
+		super();
 		this.emailChangeForm = this.fb.group({
-			email: ['s22900@pjwstk.edu.pl', [Validators.required, Validators.email]],
-			password: ['', Validators.required],
+			email: ['', [Validators.required, Validators.email]],
 		});
 	}
 
 	public onSubmit() {
-		this.snackBar.open('Pomyślnie zmieniono adres mailowy!', 'Zamknij', {
-			duration: 2000,
-		});
+		return this.service
+			.changeEmail(this.emailChangeForm.value.email)
+			.pipe(this.untilDestroyed())
+			.subscribe({
+				error: () =>
+					this.snackBar.open('Wystąpił błąd', 'Zamknij', {
+						duration: 2000,
+					}),
+				complete: () =>
+					this.snackBar.open('Pomyślnie zmieniono adres mailowy!', 'Zamknij', {
+						duration: 2000,
+					}),
+			});
 	}
 }
