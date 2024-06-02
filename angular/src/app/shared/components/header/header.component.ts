be18e8abe50e17170467fbce2837a9a7d1c09510
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-06-02 21:39:05 +0200

    Naprawa menu headera

--- angular/src/app/shared/components/header/header.component.ts
+++ angular/src/app/shared/components/header/header.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '@shared/services/auth.service';
 import { UserService } from '@shared/services/user.service';
 import { environment } from '../../../../environments/environment.prod';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { BaseComponent } from '../base/base.component';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -15,8 +14,6 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent extends BaseComponent {
 	public isUserLoggedInAsStudent = true;
 
-	public showSidenav = false;
-
 	protected baseUrl = environment.apiUrl.replace('/api', '');
 	protected user$ = this.userService.getMyProfile();
 
@@ -25,27 +22,12 @@ export class HeaderComponent extends BaseComponent {
 	constructor(
 		public authService: AuthService,
 		public userService: UserService,
-		private breakpointObserver: BreakpointObserver,
 		private translate: TranslateService
 	) {
 		super();
-		this.breakpointObserver
-			.observe(['(max-width: 1000px)'])
-			.pipe(this.untilDestroyed())
-			.subscribe((result: BreakpointState) => {
-				if (!result.matches) {
-					this.showSidenav = false;
-				}
-			});
 	}
 
 	public changeLanguage(value: string) {
 		this.translate.use(value.toLowerCase());
-	}
-
-	public hideSidenav(): void {
-		if (this.showSidenav) {
-			this.showSidenav = false;
-		}
 	}
 }
