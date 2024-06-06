ee54da3446bcd3755706dc6e1fb29b82cdd8176c
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-06-06 02:13:25 +0200

    Zmiany z #351

--- angular/src/app/real-estate/services/real-estate.service.ts
+++ angular/src/app/real-estate/services/real-estate.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { IResult } from 'src/app/offer/models/offer.models';
 import {
 	IAddProperty,
 	IAddResult,
@@ -309,10 +310,13 @@ export class RealEstateService {
 			);
 	}
 
-	public addRealEstateFiles(id: number, formData: FormData): Observable<void> {
+	public addRealEstateFiles(
+		id: number,
+		formData: FormData
+	): Observable<IResult> {
 		const headers = new HttpHeaders();
 		headers.append('enctype', 'multipart/form-data');
-		return this.httpClient.post<void>(
+		return this.httpClient.post<IResult>(
 			`${this.apiRoute}/properties/${id}/files`,
 			formData,
 			{ headers }
