d8ca1c5f486b06b93aa8f547b0fcd2daad632f28
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2024-06-14 09:24:40 +0200

    383-fe-report-problem-link-in-settings (#384)

--- angular/src/app/settings/components/help-center/help-center.module.ts
+++ angular/src/app/settings/components/help-center/help-center.module.ts
@@ -4,9 +4,17 @@ import { MatCardModule } from '@angular/material/card';
 
 import { HelpCenterComponent } from './help-center.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	declarations: [HelpCenterComponent],
-	imports: [CommonModule, MatCardModule, TranslateModule],
+	imports: [
+		CommonModule,
+		MatCardModule,
+		TranslateModule,
+		RouterLink,
+		MatButtonModule,
+	],
 })
 export class HelpCenterModule {}
