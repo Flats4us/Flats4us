2f6f66ca67f4b45b7594776f2a89601a89463ba2
Konrad Sochacki <s23801@pjwstk.edu.pl>
2024-06-20 16:29:02 +0200

    Rent opinion allow access fix

--- angular/src/app/rents/models/rents.models.ts
+++ angular/src/app/rents/models/rents.models.ts
@@ -47,6 +47,7 @@ export interface IRent {
 	propertyType: number;
 	propertyAvgRating: number;
 	mainTenantId: number;
+	IsAddingOpinionAllowed: boolean;
 	owner: IOwner;
 	propertyImages: IImage[];
 	tenants: ITenant[];
