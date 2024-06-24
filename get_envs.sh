37f6bbf30cf523ec7ba7e738dc3ffc6dfd3a64ed
mattonaly <mattioskar.mat@gmail.com>
2024-06-24 10:19:13 +0200

    update package version, rename

--- get_envs.sh
+++ get_envs.sh
@@ -5,5 +5,5 @@ cd /home/Flats4us
 COMMIT_HASH=$(git rev-parse HEAD)
 COMMIT_DATE=$(git log -1 --format=%cd --date=format:"%Y-%m-%d %H:%M:%S")
 
-sed -i "s/##COMMIT_HASH##/$COMMIT_HASH/g" angular/src/environments/environment.prod.ts
-sed -i "s/##COMMIT_DATE##/$COMMIT_DATE/g" angular/src/environments/environment.prod.ts
+sed -i "s/##COMMIT_HASH##/$COMMIT_HASH/g" Flats4us-frontend/src/environments/environment.prod.ts
+sed -i "s/##COMMIT_DATE##/$COMMIT_DATE/g" Flats4us-frontend/src/environments/environment.prod.ts
