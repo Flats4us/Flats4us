647cd4c9db5074b5f5a566de092e518562985843
giziuuu <kubagizewski@gmail.com>
2024-07-30 22:10:38 +0200

    fix: add property files

--- Flats4us/Entities/Dto/PropertyFilesDto.cs
+++ Flats4us/Entities/Dto/PropertyFilesDto.cs
@@ -4,10 +4,8 @@ namespace Flats4us.Entities.Dto
 {
     public class PropertyFilesDto
     {
-        [Required]
         public IFormFile? TitleDeed { get; set; }
 
-        [Required]
         public List<IFormFile>? Images { get; set; }
     }
 }
